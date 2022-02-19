1369be29088a6614091bbcb69141f5a2257e9275
Cooper Sullivan <thecoopersully@gmail.com>
2022-02-19 11:45:52 -0600

    Fixed scripts & added animation(s)
    - Fixed scripts
    - Removed unused scripts
    - Added moving platform script
    - Added moving platform placeholder

---
+++ Assets/Scripts/platformMovement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class platformMovement : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        throw new NotImplementedException();
+    }
+}
