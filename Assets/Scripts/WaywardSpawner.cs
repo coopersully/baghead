5918ba18c975b37c75b4f7304b903180e9c9bcbc
Cooper Sullivan <thecoopersully@gmail.com>
2022-02-19 09:39:50 -0600

    Added WaywardSpawner & Cooper Scene
    - Fixed .gitignore to add .idea
    - Added WaywardSpawner.cs
    - Created "Cooper" Scene

---
+++ Assets/Scripts/WaywardSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class WaywardSpawner : MonoBehaviour
+{
+    public float spawnInterval = 5.0f;
+    private GameObject[] _spawnpoints;
+    public GameObject wayward;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _spawnpoints = GameObject.FindGameObjectsWithTag("WaywardSpawnpoint");
+        StartCoroutine(SpawnCoroutine());
+    }
+
+    IEnumerator SpawnCoroutine()
+    {
+        while (true)
+        {
+            Instantiate(wayward, _spawnpoints[Random.Range(0, _spawnpoints.Length)].gameObject.transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+}
