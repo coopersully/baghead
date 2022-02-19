67945a85f87e530bdb287075af6e00fd9418c4e6
BridgetHooks116 <bfranhooks@gmail.com>
2022-02-19 14:58:50 -0600

    Camera correct
    camera follow, enemy spawn added. Correcting enemy spawn

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+ public Transform target;
+ 
+ public double smoothSpeed = 0.125;
+ public Vector3 offset;
+
+ void LateUpdate()
+ {
+  transform.position = target.position + offset;
+ }
+ 
+ 
+ 
+}
--- Assets/Scripts/waywardSpawner.cs
+++ Assets/Scripts/waywardSpawner.cs
@@ -1,25 +1,29 @@
 using System.Collections;
 using UnityEngine;
 
-public class waywardSpawner : MonoBehaviour
+public class WaywardSpawner : MonoBehaviour
 {
-    public float spawnInterval = 5.0f;
-    private GameObject[] _spawnpoints;
+    //public float spawnInterval = 10.0f;
+    private GameObject[] _spawnpoint;
     public GameObject wayward;
+
+    public int xPos;// Bridget added
+    public int yPos;// Bridget added
     
     // Start is called before the first frame update
     void Start()
     {
-        _spawnpoints = GameObject.FindGameObjectsWithTag("WaywardSpawnpoint");
+        _spawnpoint = GameObject.FindGameObjectsWithTag("WaywardSpawnpoint");
         StartCoroutine(SpawnCoroutine());
     }
 
     IEnumerator SpawnCoroutine()
-    {
+    { 
         while (true)
         {
-            Instantiate(wayward, _spawnpoints[Random.Range(0, _spawnpoints.Length)].gameObject.transform.position, Quaternion.identity);
-            yield return new WaitForSeconds(spawnInterval);
+            //Instantiate(wayward, _spawnpoint[Random.Range(0, _spawnpoint.Length)].gameObject.transform.position, Quaternion.identity);
+            xPos = Random.Range(0, 50);
+            yield return new WaitForSeconds(10);
         }
     }
 }
