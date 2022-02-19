43895159608985dfd2ef19a239950e5f8f0d5502
Cooper Sullivan <thecoopersully@gmail.com>
2022-02-19 12:07:40 -0600

    Optimized platformMovement.cs

--- Assets/Scripts/platformMovement.cs
+++ Assets/Scripts/platformMovement.cs
@@ -1,19 +1,21 @@
-﻿using System;
-using Cinemachine;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class platformMovement : MonoBehaviour
 {
+    // Player gets onto platform
     private void OnCollisionEnter(Collision collision)
     {
+        // Bring player with the platform (sync movement)
         if (collision.gameObject.CompareTag("Player"))
         {
             collision.gameObject.transform.parent = transform;
         }
     }
 
+    // Player exits the platform
     private void OnCollisionExit(Collision collision)
     {
+        // Reset movement sync
         if (collision.gameObject.CompareTag("Player"))
         {
             collision.gameObject.transform.parent = null;
