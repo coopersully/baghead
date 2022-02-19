77d6601ce79397a588ae82a5f1e1dd43abac6e5a
Madison Pridgen <madison.pridgen3@gmail.com>
2022-02-19 15:23:38 -0600

    deathfloor

---
+++ Assets/Scripts/killFloor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killFloor : MonoBehaviour
+{
+    [SerializeField] 
+    private Transform player;
+    [SerializeField] 
+    private Transform respawnPoint;
+
+    private void OnTriggerEnter(Collider other) {
+        player.transform.position = respawnPoint.transform.position;
+    }
+
+}
