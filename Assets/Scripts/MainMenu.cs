3f340f2695bcec1e95cdf98f800ae1476dabb920
Cooper Sullivan <thecoopersully@gmail.com>
2022-02-20 10:28:17 -0600

    Added Loading Screen

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,10 +4,6 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public AudioSource clickSound;
-    public void StartGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
 
     public void QuitGame()
     {
