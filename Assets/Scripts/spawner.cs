d700d4fb4c694872e0200ce6cac7ea261c8df884
Cooper Sullivan <thecoopersully@gmail.com>
2022-02-20 14:35:06 -0600

    Fixed playerDeath.cs* and spawner.cs

--- Assets/Scripts/spawner.cs
+++ Assets/Scripts/spawner.cs
@@ -3,14 +3,16 @@ using UnityEngine;
 
 public class spawner : MonoBehaviour
 {
+    public bool isRandom = true;
+    public string waypointTag = "WaywardSpawnpoint";
     public float spawnInterval = 5.0f;
     private GameObject[] _spawnpoints;
-    public GameObject wayward;
+    public GameObject mob;
     
     // Start is called before the first frame update
     void Start()
     {
-        _spawnpoints = GameObject.FindGameObjectsWithTag("WaywardSpawnpoint");
+        _spawnpoints = GameObject.FindGameObjectsWithTag(waypointTag);
         StartCoroutine(SpawnCoroutine());
     }
 
@@ -18,7 +20,19 @@ public class spawner : MonoBehaviour
     {
         while (true)
         {
-            Instantiate(wayward, _spawnpoints[Random.Range(0, _spawnpoints.Length)].gameObject.transform.position, Quaternion.identity);
+            if (isRandom)
+            {
+                // Spawn a NPC at a random spawnpoint
+                Instantiate(mob, _spawnpoints[Random.Range(0, _spawnpoints.Length)].gameObject.transform.position, Quaternion.identity);
+            }
+            else
+            {
+                // Spawn a NPC at every spawnpoint
+                foreach (var spawnpoint in _spawnpoints)
+                {
+                    Instantiate(mob, spawnpoint.gameObject.transform.position, Quaternion.identity);
+                }
+            }
             yield return new WaitForSeconds(spawnInterval);
         }
     }
