bb500c7ce8d60286042ff44db0ca24aa0bd9f174
Madison Pridgen <madison.pridgen3@gmail.com>
2022-02-19 19:39:46 -0600

    Oxygen Counter
    Add playerOxy.cs

---
+++ Assets/Scripts/playerOxy.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+
+
+public class PlayerOxy : MonoBehaviour
+{
+
+    private int _oxygenTotal = 60;
+    public TextMeshProUGUI oxygenAmount;
+
+    void Start()
+    {
+        RefreshTMP();
+        StartCoroutine(OxygenMeter());
+    }
+
+    IEnumerator OxygenMeter()
+    {
+        while (_oxygenTotal <= 60)
+        {
+            _oxygenTotal--;
+            RefreshTMP();
+            yield return new WaitForSeconds(1);
+        }
+
+        //add a way for when the capsules are collected add to oxygen
+        print("No more air.");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("oxygen"))
+        {
+            collision.gameObject.SetActive(false);
+            OxygenCollect();
+        }
+    }
+
+    private void OxygenCollect()
+    {
+        _oxygenTotal = 60;
+        RefreshTMP();
+    }
+
+    private void RefreshTMP()
+    {
+        oxygenAmount.SetText("Oxygen: " + _oxygenTotal + "/60");
+    }
+}
