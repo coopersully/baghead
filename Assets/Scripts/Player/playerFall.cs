e73fe43235f2ac571af18d502c15972ac44e5737
Cooper Sullivan <thecoopersully@gmail.com>
2022-02-20 14:58:26 -0600

    Updated LVLOne_Latest.unity and fixed killFloor.cs

---
+++ Assets/Scripts/Player/playerFall.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class playerFall : MonoBehaviour
+{
+    
+    private GameObject _respawnPoint;
+
+    private void Start()
+    {
+        _respawnPoint = GameObject.FindGameObjectWithTag("Respawn");
+    }
+
+    private void Update()
+    {
+        // If the player crosses below -5 on the y-axis
+        if (gameObject.transform.position.y < -5.0f)
+        {
+            gameObject.transform.SetPositionAndRotation(_respawnPoint.transform.position, Quaternion.identity);
+        }
+    }
+
+}
