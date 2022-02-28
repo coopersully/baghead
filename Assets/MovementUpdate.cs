83dc304967d26545f490a269a51ebad7b4aed827
BridgetHooks116 <bfranhooks@gmail.com>
2022-02-28 11:02:15 -0600

    Level 2
    All level two copies deleted. Level two organized, rotations and scale corrected, and new models added. Enemy patrol and follow scripts created.

---
+++ Assets/MovementUpdate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementUpdate : MonoBehaviour
+{
+   public float speed = .1f;
+  
+
+   private void Update()
+   {
+      float xDirection = Input.GetAxis("Horizontal");
+      float yDirection = Input.GetAxis("Vertical");
+      Vector3 moveDirection = new Vector3(xDirection,yDirection, 0);
+      transform.position += moveDirection * speed;
+   }
+}
---
+++ Assets/Scripts/UpdateMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Update_Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += new Vector3(0,+1); 
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position += new Vector3(0,-1); 
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += new Vector3(-1,0); 
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += new Vector3(+1,0); 
+        }
+    }//end update
+}//end class
