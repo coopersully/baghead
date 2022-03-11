faa99f7898e250c109efd1af37cd3c5a1a983150
BridgetHooks116 <bfranhooks@gmail.com>
2022-03-11 14:47:12 -0600

    platform change
    size change level 2

---
+++ Assets/Scripts/Player/newMov.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class newMov : MonoBehaviour
+{
+     [SerializeField] private float moveSpeed = 10f;
+
+     private CharacterController _characterController;
+     public bool isGrounded;
+
+     private void Awake() => _characterController = GetComponent<CharacterController>();
+
+     private void FixedUpdate()
+     {
+          float horizontal = Input.GetAxis("Horizontal");
+          float vertical = Input.GetAxis("Vertical");
+
+          Vector3 direction = new Vector3(horizontal, 0, vertical);
+          Vector3 movement = transform.TransformDirection(direction) * moveSpeed;
+
+          isGrounded = _characterController.SimpleMove(movement);
+     }
+}
