c6cc3ca64adaca4168b9a4351efcfadcfb14ce86
BridgetHooks116 <bfranhooks@gmail.com>
2022-03-12 10:34:53 -0600

    Enemy scripts, organization
    Enemy patrol and follow added, enemy scripts organized into a folder, and player respawn upon water hit added

---
+++ Assets/Scripts/enemy/enemyFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyFollow : MonoBehaviour
+{
+    //Attach this script to your enemy; enemy will run at player
+    public GameObject player;
+    public int speed;
+    void Update()
+    {
+        Transform transform1;
+        (transform1 = transform).LookAt(player.transform);
+        transform1.position+= transform1.forward * speed *Time.deltaTime;
+    }
+}//end class
---
+++ Assets/Scripts/enemy/enemyPatrol.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyPatrol : MonoBehaviour
+{
+    //this script creates an array of points for the enemy to patrol between. Simply attach script to enemy
+    public Transform[] wayPoints; 
+    public int speed;// choose enemy patrol speed
+
+    private int _waypointIndex;
+    private float _distance;
+    
+    void Start()
+    {
+        _waypointIndex = 0;
+        transform.LookAt(wayPoints[_waypointIndex].position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _distance = Vector3.Distance(transform.position, wayPoints[_waypointIndex].position);
+        if (_distance < 1f)
+        {
+            IncreaseIndex();
+        }
+        Patrol();
+    }//update over
+
+    void Patrol()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    void IncreaseIndex()
+    {
+        _waypointIndex++;
+        if (_waypointIndex >= wayPoints.Length)
+        {
+            _waypointIndex = 0;
+        }
+        transform.LookAt(wayPoints[_waypointIndex].position);
+    }
+}
