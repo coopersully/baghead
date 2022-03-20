8f2f8d944c8eab937f70c3cdcc428b0673104fb6
BridgetHooks116 <bfranhooks@gmail.com>
2022-03-20 14:02:46 -0700

    LVL two done for now
    enemies added

--- Assets/Scripts/enemy/enemyFollow.cs
+++ Assets/Scripts/enemy/enemyFollow.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,10 +8,20 @@ public class enemyFollow : MonoBehaviour
     //Attach this script to your enemy; enemy will run at player
     public GameObject player;
     public int speed;
-    void Update()
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Transform transform1;
+            (transform1 = transform).LookAt(player.transform);
+            transform1.position+= transform1.forward * speed *Time.deltaTime;
+        }
+    }// end trigger statement 
+    /*void Update()
     {
         Transform transform1;
         (transform1 = transform).LookAt(player.transform);
         transform1.position+= transform1.forward * speed *Time.deltaTime;
-    }
+    }*/
 }//end class
---
+++ Assets/Scripts/enemy/kill.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kill : MonoBehaviour
+{
+    public GameObject Player;
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (gameObject.CompareTag("Player"))
+        {
+            Player.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
